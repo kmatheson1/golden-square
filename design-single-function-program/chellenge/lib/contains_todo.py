f27af3c906a104a96f4c29856ea03981f1867cd9
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-09-20 09:26:22 +0100

    design single function challenge complete

--- design-single-function-program/chellenge/lib/contains_todo.py
+++ design-single-function-program/chellenge/lib/contains_todo.py
@@ -1,8 +1,13 @@
 # File: lib/contains_todo.py
 
 def contains_todo(text):
-    words = text.split()
-    if '#TODO' in words:
-        return "Your text contains #TODO."
-    else:
-        return "Your text does not contain #TODO."
+    if text == "":
+        raise Exception("Cannot check if text contains #TODO if no text is given.")
+    
+    filtered_words = []
+    for word in text.split():
+        filtered_letters = [letter for letter in word if letter.isalpha() or letter == '#']
+        filtered_word = ''.join(filtered_letters)
+        filtered_words.append(filtered_word)
+
+    return "Your text contains #TODO." if '#TODO' in filtered_words else "Your text does not contain #TODO."
--- design-single-function-program/chellenge/tests/test_contains_todo.py
+++ design-single-function-program/chellenge/tests/test_contains_todo.py
@@ -1,5 +1,6 @@
 # File tests/test_contains_todo.py
 from lib.contains_todo import contains_todo
+import pytest
 
 """
 If given a string that does not contain '#TODO'
@@ -22,7 +23,23 @@ If given a string where '#TODO' is in the text but not the first word
 return string confirming that text does contain '#TODO'
 """
 def test_text_contains_todo_any_position():
-    result = contains_todo(" Take the #TODO bins out.")
+    result = contains_todo("Take the #TODO bins out.")
     assert result == "Your text contains #TODO."
 
-#end of day, finishing tomorrow
+"""
+If given a string where '#TODO' is directly followed by punctuation e.g. '#TODO:'
+return string confirming that text does contain '#TODO'
+"""
+def test_text_contains_todo_with_punct():
+    result = contains_todo("#TODO: Take the bins out.")
+    assert result == "Your text contains #TODO."
+
+"""
+If given an empty string
+Return error message
+"""
+def test_empty_string_returns_error():
+    with pytest.raises(Exception) as err:
+        contains_todo("")
+    error_message = str(err.value)
+    assert error_message == "Cannot check if text contains #TODO if no text is given."
